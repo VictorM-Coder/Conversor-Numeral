1841602920c9881355feae5ca593a9904908c0a9
Victor Martins <martinsvictor6781@gmail.com>
2021-10-14 10:11:05 -0300

    Ajustes e melhoria do código
    tornando o código mais limpo e preparando alguns sub-métodos para conversão.

--- code/src/dominio/Conversor.java
+++ code/src/dominio/Conversor.java
@@ -2,16 +2,12 @@ package dominio;
 
 public class Conversor {
 
-    protected   Numero fromDecimal(Numero numeroDecimal, int base){
-        int dividendo = Integer.parseInt(numeroDecimal.getValor());
-        String restos = "";
-
-        while(dividendo >= base){
-            restos += Integer.toString(dividendo%base);
-            dividendo/=base;
+    protected   Numero fromDecimal(Numero numeroDecimal, int baseSaida){
+        if (baseSaida <= 9){
+            return fromDecimalBaseMenorQue10(numeroDecimal, baseSaida);
+        }else{
+            return fromDecimalBaseMaiorQue10(numeroDecimal, baseSaida);
         }
-
-        return  new Numero(inverterString(restos), base);
     }
 
     protected Numero toDecimal(){
@@ -25,8 +21,26 @@ public class Conversor {
         for(int cont = (stringBase.length()-1) ; cont >= 0; cont--){
             stringInverse += "" + stringBase.charAt(cont);
         }
-
         return stringInverse;
+
     }
 
+    private Numero fromDecimalBaseMenorQue10(Numero numeroDecimal, int baseSaida){
+        int dividendo = Integer.parseInt(numeroDecimal.getValor());
+        String restos = "";
+
+        while(dividendo >= baseSaida){
+            restos += Integer.toString(dividendo%baseSaida);
+            dividendo/=baseSaida;
+        }
+
+        return  new Numero(inverterString(restos), baseSaida);
+    }
+
+    private Numero fromDecimalBaseMaiorQue10(Numero numeroDecimal, int baseSaida){
+        return null;
+    }
+    private int charToDecimal(char letra){
+        return (int) (letra - 55);
+    }
 }
--- code/src/teste/TesteConversor.java
+++ code/src/teste/TesteConversor.java
@@ -1,16 +1,20 @@
 package teste;
 
 import dominio.Calculadora;
+import dominio.Conversor;
 import dominio.Numero;
 
 public class TesteConversor {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         Numero numero1 = new Numero("20",10);
+        Conversor c1 = new Conversor();
 
         Numero numCovertido = calculadora.converter(numero1, 2);
 
         System.out.println("-----------");
-        System.out.println(numCovertido.getValor());
+        System.out.println();
+
+
     }
 }
